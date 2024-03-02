0fa15c994ffee2ca0d0ce8d880759bd760694a9c
m_hafidh_s <hafidhsanjaya2@gmail.com>
2024-03-02 12:31:52 +0700

    Add negative case di Login Scenario

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -33,4 +33,120 @@ public class Login extends env_target {
         );
         driver.quit();
     }
+    @Test
+    public void WrongUsername(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_secret");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void WrongPassword(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_user");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void EmptyDataInput(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void EmptyUsername(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void EmptyPassowrd(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3"))
+        );
+        driver.quit();
+    }
 }
