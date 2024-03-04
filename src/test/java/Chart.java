3fd1e35f16c9a8702450f989ef5473fab415ef64
m_hafidh_s <hafidhsanjaya2@gmail.com>
2024-03-04 20:49:25 +0700

    Add new test script checkout

--- src/test/java/Chart.java
+++ src/test/java/Chart.java
@@ -8,7 +8,7 @@ import java.sql.Driver;
 import java.time.Duration;
 public class Chart extends env_target{
     @Test
-    public void remove_product(){
+    public void success_remove_product(){
         //SET DRIVER LOCATION PATH
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 
@@ -46,7 +46,7 @@ public class Chart extends env_target{
     }
 
     @Test
-    public void chechout_product(){
+    public void success_checkout_product(){
         //SET DRIVER LOCATION PATH
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 
@@ -90,17 +90,178 @@ public class Chart extends env_target{
         wait.until(
                 ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"checkout_complete_container\"]/h2"))
         );
-
-
-
-
-
-
         driver.quit();
     }
 
     @Test
-    public void checkout_empty_product(){
+    public void failed_checkout_product_empty_data_information_form(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void failed_checkout_product_empty_firstname_information_form(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"last-name\"]")).sendKeys("Maguire");
+        driver.findElement(By.xpath("//*[@id=\"postal-code\"]")).sendKeys("3325125");
+        driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void failed_checkout_product_empty_lastname_information_form(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys("Harry");
+        driver.findElement(By.xpath("//*[@id=\"postal-code\"]")).sendKeys("3325125");
+        driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]"))
+        );
+        driver.quit();
+    }
+    @Test
+    public void failed_checkout_product_empty_zippostalcode_information_form(){
+        //SET DRIVER LOCATION PATH
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+
+        //MAXIMIZE DRIVER
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //SET URL
+        driver.get(baseUrl);
+        Duration duration = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"login-button\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header_container\"]/div[2]/span"))
+        );
+        driver.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys("Harry");
+        driver.findElement(By.xpath("//*[@id=\"last-name\"]")).sendKeys("Maguire");
+        driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+        wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[1]/div[4]"))
+        );
+        driver.quit();
+    }
+
+    @Test
+    public void failed_checkout_empty_product(){
         //SET DRIVER LOCATION PATH
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 
